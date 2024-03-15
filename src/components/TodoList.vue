059371ec72101ec4143956b638f72552e6db13d4
Filin Andrey <Andrei_Filin@rntgroup.com>
2024-03-15 19:52:00 +0300

    add checkbox and button

---
+++ src/components/TodoList.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <ul>
+      <Todoitems
+      v-for="todo of todos"
+      v-bind:todo="todo"
+      v-on:remove-todo="removeTodo"
+      />
+    </ul>
+  </div>
+</template>
+
+<script>
+import Todoitems from '@/components/Todoitems'
+export default {
+  props: ['todos'],
+  components: {
+    Todoitems
+  },
+  methods: {
+    removeTodo (id) {
+      this.$emit('remove-todo', id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+</style>
